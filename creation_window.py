a8e194eda556ade01bd8861e6b329a116f7507c1
Dragonfly774 <romanov7semen@ya.ru>
2020-11-08 23:21:02 +0300

    открывается окно ты пишешь текст и оно сохраняется в бд

--- creation_window.py
+++ creation_window.py
@@ -1,26 +1,37 @@
 # -*- coding: utf-8 -*-
 import sys
+import sqlite3
+import datetime as dt
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from design_window_creation import Ui_MainWindow_cr
-from main import save_notes_data
+
+category = {1: 'встреча', 2: 'цели', 3: 'сегодня', 4: 'важное', 5: 'нет'}
 
 
 class MyWidget(QMainWindow, Ui_MainWindow_cr):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.pushButton.clicked.connect(self.save_psh)
-        # self.pushButton_2.clicked.connect(self.save_psh)
+        self.comboBox_choice.addItems([category[1], category[2], category[3], category[4], category[5]])
+        self.pushButton_2.clicked.connect(self.save_psh)
 
     def save_psh(self):
         self.saving_notes_to_database()
 
     def saving_notes_to_database(self):
         info = self.textEdit.toPlainText()
-        print(info)
-        self.lineEdit.setText(info)
+        value_comboBox = self.comboBox_choice.currentText()
+        for i, j in category.items():
+            if value_comboBox == category.get(i):
+                value_comboBox = i
         if info:
-            save_notes_data(info)
+            con = sqlite3.connect('project_db.db')
+            cur = con.cursor()
+            datetime = dt.datetime.now()
+            cur.execute(f"INSERT INTO Data(section, data, datetime, category) VALUES(1, ?, ?, ?)",
+                        (info, datetime, value_comboBox)).fetchall()
+            con.commit()
+            con.close()
 
 
 # def main():
